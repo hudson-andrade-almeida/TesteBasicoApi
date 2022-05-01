91cba639bd271a5961adec4a451a9ab9bdbc76ef
hudson-andrade-almeida <hudson.tuta@hotmail.com>
2022-04-30 21:33:29 -0300

    first commit

---
+++ MyCar/Context/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MyCar.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCar.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", false, true)
+            .Build();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("ServerConnection"));
+        }
+    }
+}
---
+++ MyCar/Controllers/CarsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCar.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCar.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class CarsController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CarsController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+
+        []
+        public async Task<IActionResult> GetCars()
+        {
+            return Ok
+            (new
+            {
+                sucess = true,
+                data = await _appDbContext.Cars.ToListAsync()
+            });      
+        }
+    }
+}
